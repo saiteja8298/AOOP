e64b6d219b479e31e6a349118fba8685ed4de9e3
saiteja8298 <asaitejar@gmail.com>
2024-09-25 10:54:54 +0530

    Delete lab_6a directory

--- lab_6a/src/com/example/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Main {
-	    public static void main(String[] args) {
-	        TaskManagementSystem system = new TaskManagementSystem();
-	        system.run();
-	    }
-	}
